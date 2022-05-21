73fd0a7a9fa6d98600a685ed90bf8fbdd151bbea
Shomuhammad <zolborsovzolbors07@gmail.com>
2022-05-21 17:47:02 +0600

    Initial commit

---
+++ app/src/main/java/com/example/dz12/Contact.java
@@ -0,0 +1,15 @@
+package com.example.dz12;
+
+public class Contact {
+    private String name, number;
+    public Contact(String name , String number){
+        this.name = name ;
+        this.number = number;
+    }
+    public String getName() {
+        return name;
+    }
+    public String getNumber() {
+        return number;
+    }
+}
